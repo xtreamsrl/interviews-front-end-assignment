cd29574ea88ff4cad3ca9c0d8087d3184afe20dc
RainbowWZRD <bucciolsimone@gmail.com>
2022-01-22 12:08:21 +0100

    backend: fetch data from firestore

--- xtream-posts/src/App.js
+++ xtream-posts/src/App.js
@@ -1,12 +1,28 @@
+import { collection, getDocs } from "firebase/firestore";
 import { useState, useEffect } from "react";
-import "./App.css";
+import { db } from "./lib/firebase";
+import Post from "./components/Post";
 
 function App() {
-  const [post, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const postsCollectionRef = collection(db, "posts");
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPosts(data.docs.map((post) => ({ ...post.data(), id: post.id })));
+    };
+    getPosts();
+  }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      {posts.map((post) => {
+        const { body, title, id } = post;
+        return <Post id={id} title={title} body={body} />;
+      })}
+    </div>
+  );
 }
 
 export default App;
