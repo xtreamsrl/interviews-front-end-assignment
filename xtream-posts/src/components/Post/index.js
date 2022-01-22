b953cb0c7c31a63bba8fcf94ad78eb53f921fe88
RainbowWZRD <bucciolsimone@gmail.com>
2022-01-22 15:55:37 +0100

    feature: add update post

--- xtream-posts/src/components/Post/index.js
+++ xtream-posts/src/components/Post/index.js
@@ -1,11 +1,43 @@
+import { useState } from "react";
 import style from "./index.module.css";
 import { capitalizeFirstLetter } from "../../utils/capitalizeFirstLetter";
+import { db } from "../../lib/firebase";
+import { doc, updateDoc } from "firebase/firestore";
 
 export default function Post({ id, title, body }) {
   return (
     <li className={style["post-card"]} key={id}>
       <h1>{capitalizeFirstLetter(title)}</h1>
       <h2>{capitalizeFirstLetter(body)}</h2>
+      <UpdateTitle id={id} />
     </li>
   );
 }
+
+export const UpdateTitle = ({ id }) => {
+  const [updatedTitle, setUpdatedTitle] = useState("");
+  const [updatedBody, setUpdatedBody] = useState("");
+
+  const updatePost = async (id) => {
+    const postDoc = doc(db, "posts", id);
+    const newPost = { title: updatedTitle, body: updatedBody };
+    await updateDoc(postDoc, newPost);
+    window.location.reload();
+    console.log(newPost, "<<<NEW POST");
+  };
+
+  const handleUpdatedTitle = (e) => {
+    setUpdatedTitle(e.target.value);
+  };
+
+  const handleUpdatedBody = (e) => {
+    setUpdatedBody(e.target.value);
+  };
+  return (
+    <>
+      <input placeholder="Update Title" onChange={handleUpdatedTitle} />
+      <input placeholder="Update Body" onChange={handleUpdatedBody} />
+      <button onClick={() => updatePost(id)}>Update</button>
+    </>
+  );
+};
