74833afb2574781849084d5e6a2e29f6751e8ae9
Luigi Lesca <luigilesca@hotmail.it>
2022-10-03 00:09:38 +0200

    edit home  page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,86 +1,127 @@
-import axios from 'axios';
+import { deleteDoc, doc } from 'firebase/firestore';
+//
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { selectPosts, storeProducts } from '../../store/slice/postSlice';
 import { useSelector } from 'react-redux';
-import { Posts, Loader, Pagination } from '../../components';
-import { selectPost } from '../../store/slice/slicePost';
+
+import useFetchCollection from '../../customHook/useFetchCollection';
+
+import { Loader, Pagination } from '../../components';
 
 import styles from './Home.module.scss';
 import { toast } from 'react-toastify';
+import { db } from '../../firebase/config';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+import Notiflix from 'notiflix';
 
 const Home = () => {
-  const [allPosts, setAllPosts] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const { data, isLoading } = useFetchCollection('posts');
+  //
+  const dispatch = useDispatch();
 
-  const arr = useSelector(selectPost);
+  const posts = useSelector(selectPosts);
+
+  useEffect(() => {
+    dispatch(
+      storeProducts({
+        posts: data,
+      })
+    );
+  }, [data, dispatch]);
 
   ///////////////////////////////////////////////////////////////////
-  //PAGINATION STATE
-  const totalPosts = Object.keys(allPosts).length;
+  // PAGINATION STATE
 
   const [currentPage, setCurrentPage] = useState(1);
   const [postPerPage] = useState(5);
 
   const indexOfLastPost = currentPage * postPerPage;
   const indexOfFirstPost = indexOfLastPost - postPerPage;
-  const currentProduct = allPosts.slice(indexOfFirstPost, indexOfLastPost);
+  const currentProduct = posts.slice(indexOfFirstPost, indexOfLastPost);
 
-  let listPosts = arr.concat(currentProduct);
-  ///////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////
 
-  const fetchAllPosts = async () => {
-    setIsLoading(true);
+  const deletePost = async (id) => {
     try {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
-
-      const data = response.data;
-
-      setIsLoading(false);
-      setAllPosts(data);
+      await deleteDoc(doc(db, 'posts', id));
+      toast.success('Post deleted successfully.');
     } catch (error) {
-      console.error(error);
-      toast.error('Something went wrong...');
+      toast.error(error.message);
     }
   };
 
-  useEffect(() => {
-    fetchAllPosts();
-  }, []);
+  //Notiflix
+  const confirmDelete = (id) => {
+    Notiflix.Confirm.show(
+      'Delete Post',
+      'You are about to delete this post?',
+      'Delete',
+      'Cancel',
+      function okCb() {
+        deletePost(id);
+      },
+      function cancelCb() {
+        console.log('');
+      },
+      {
+        width: '320px',
+        borderRadius: '8px',
+        titleColor: 'orangered',
+        okButtonBackground: 'orangered',
+        cssAnimationStyle: 'zoom',
+      }
+    );
+  };
 
   return (
-    <div className={styles.container}>
-      <h1>All Posts</h1>
+    <>
+      {isLoading && <Loader />}
+      <div className={styles.table}>
+        <h2>Resume posts</h2>
 
-      <div>
-        <div>
-          {!isLoading ? (
-            listPosts.map((post) => {
-              const { body, id, title, userId, comments } = post;
+        {posts.length === 0 ? (
+          <p>No products found.</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>E-mail</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentProduct.map((post) => {
+                const { id, name, email } = post;
 
-              return (
-                <div key={id} className={styles.posts}>
-                  <Posts
-                    post={post}
-                    title={title}
-                    body={body}
-                    id={id}
-                    userId={userId}
-                    comments={comments}
-                  />
-                </div>
-              );
-            })
-          ) : (
-            <Loader />
-          )}
-        </div>
+                return (
+                  <tr key={id}>
+                    <td>{name}</td>
+                    <td>{email}</td>
+
+                    <td className={styles.icons}>
+                      <Link to={`/add-post/${id}`}>
+                        <FaEdit size={20} color='green' />
+                      </Link>
+                      &nbsp; &nbsp;
+                      <FaTrash size={20} color='red' onClick={() => confirmDelete(id)} />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
         <Pagination
           postPerPage={postPerPage}
           setCurrentPage={setCurrentPage}
           currentPage={currentPage}
-          totalPosts={totalPosts}
+          totalPosts={posts.length}
         />
       </div>
-    </div>
+    </>
   );
 };
 
