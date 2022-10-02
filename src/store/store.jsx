c24f0fcafc92c4ac15c8c8cb023009e9aaa03afa
Luigi Lesca <luigilesca@hotmail.it>
2022-10-03 00:11:59 +0200

    edit store, add favouriteSlice and postSlice

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,13 +1,19 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
-import postReducer from './slice/slicePost';
+import postReducer from './slice/postSlice';
+import favouriteReducer from './slice/favouriteSlice';
 
 const rootReducer = combineReducers({
   post: postReducer,
+  favourite: favouriteReducer,
 });
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store;
