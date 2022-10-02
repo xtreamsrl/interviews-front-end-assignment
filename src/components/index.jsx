38cb920f3d09ada757c774c34d52bae1c5ae584d
Luigi Lesca <luigilesca@hotmail.it>
2022-10-03 00:04:41 +0200

    edit index component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,6 @@
 export { default as Navbar } from './navbar/Navbar';
 export { default as SinglePost } from './singlePost/SinglePost';
 export { default as Posts } from './post/Posts';
-export { default as ShowButton } from './showButton/ShowButton';
-export { default as AddFavouriteButton } from './addFavouriteButton/AddFavouriteButton';
 export { default as Loader } from './loader/Loader';
 export { default as Pagination } from './pagination/pagination';
+export { default as PostsList } from './postsList/PostsList';
