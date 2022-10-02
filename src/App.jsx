f7ffb140136de3b16d6abc4d39b8fc4c3b6e005e
Luigi Lesca <luigilesca@hotmail.it>
2022-10-02 23:43:30 +0200

    edit app dile

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { Navbar, Posts } from './components/index';
+import AddPost from './pages/addPost/AddPost';
+import { Home, Favourite, EditPost } from './pages/index';
+
+import { ToastContainer } from 'react-toastify';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <ToastContainer />
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+
+        <Route path='post/' element={<Posts />} />
+
+        <Route path='add-post/' element={<AddPost />} />
+
+        <Route path='edit/:id' element={<EditPost />} />
+
+        <Route path='/favourite' element={<Favourite />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
