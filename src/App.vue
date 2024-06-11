042031f7378cd3aebade17e7f032d058cb79ef75
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-06-11 13:17:37 +0200

    feat: create App.vue file and import AppHeader,AppMain,AppFooter components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
-<script></script>
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
 
-<template></template>
+export default {
+  data() {
+    return {};
+  },
 
-<style scoped></style>
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
+
+<style lang="scss">
+@use "./scss/style.scss";
+</style>
