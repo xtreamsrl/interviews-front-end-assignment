468c9242ec05a4025da4cab36d06ff2b3e41396c
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-06-11 13:18:20 +0200

    feat: marke navbar with searchbar in AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="h2">RecipeBook</a>
+      <form class="d-flex" role="search">
+        <input
+          class="form-control me-2 rounded-pill"
+          type="search"
+          placeholder="Search for recipes"
+          aria-label="Search"
+        />
+        <button class="btn btn-orange rounded-pill" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+.navbar {
+  height: 80px;
+}
+</style>
