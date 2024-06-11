f8dd8ac383911b527bdb7f65545aebc8f26483ad
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-06-11 13:26:52 +0200

    feat: add reactive store, and apiUri into store\index.js

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { reactive } from "vue";
+
+export const api = {
+  recipes: "http://localhost:8080/recipes",
+  comments: "http://localhost:8080/comments",
+  cuisines: "http://localhost:8080/cuisines",
+  diets: "http://localhost:8080/diets",
+  difficulties: "http://localhost:8080/difficulties",
+  ingredients: "http://localhost:8080/ingredients",
+};
+
+export const store = reactive({});
