d18e433154f785d1ce50264e01bc54ebb3ebc527
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-06-11 18:57:38 +0200

    feat: RecipeDetailPage layout

--- src/pages/RecipeDetailPage.vue
+++ src/pages/RecipeDetailPage.vue
@@ -1,16 +1,108 @@
 <script>
+import axios from "axios";
+import { store, api } from "../store/index";
+
+import AppHeader from "../components/AppHeader.vue";
+import AppMainRecipeDetail from "../components/AppMainRecipeDetail.vue";
+import AppFooter from "../components/AppFooter.vue";
+
 export default {
   data() {
-    return {};
+    return {
+      recipe: {},
+      comments: [],
+      cuisines: [],
+      difficulties: [],
+      diets: [],
+    };
+  },
+
+  components: {
+    AppHeader,
+    AppMainRecipeDetail,
+    AppFooter,
+  },
+
+  computed: {
+    recipeId() {
+      return this.$route.params.id;
+    },
+
+    baseUrl() {
+      return api.recipes + "/" + this.recipeId;
+    },
+  },
+
+  methods: {
+    fetchRecipeDetails() {
+      axios.get(this.baseUrl).then((response) => {
+        // console.log(response);
+        this.recipe = response.data;
+      });
+    },
+    fetchComments() {
+      axios.get(api.comments).then((response) => {
+        response.data.forEach((comment) => {
+          if (comment.recipeId == this.recipeId) {
+            // console.log(comment);
+            this.comments.push(comment);
+          }
+        });
+      });
+    },
+    fetchCuisines() {
+      axios.get(api.cuisines).then((response) => {
+        response.data.forEach((cuisine) => {
+          if (cuisine.id == this.recipe.cuisineId) {
+            // console.log(cuisine);
+            this.cuisines.push(cuisine);
+          }
+        });
+      });
+    },
+    fetchDifficulties() {
+      axios.get(api.difficulties).then((response) => {
+        response.data.forEach((difficulty) => {
+          //   console.log(difficulty);
+          if (difficulty.id == this.recipe.difficultyId) {
+            this.difficulties.push(difficulty);
+          }
+        });
+      });
+    },
+    fetchDiets() {
+      axios.get(api.diets).then((response) => {
+        response.data.forEach((diet) => {
+          //   console.log(diet);
+          if (diet.id == this.recipe.dietId) {
+            this.diets.push(diet);
+          }
+        });
+      });
+    },
+  },
+
+  created() {
+    this.fetchRecipeDetails();
+    this.fetchComments();
+    this.fetchCuisines();
+    this.fetchDifficulties();
+    this.fetchDiets();
   },
 };
 </script>
 
 <template>
   <div class="container">
-    <div class="row">
-      <h1>{{ recipe.name }}</h1>
-    </div>
+    <AppHeader />
+    <AppMainRecipeDetail
+      :recipe="recipe"
+      :comments="comments"
+      :cuisines="cuisines"
+      :difficulties="difficulties"
+      :diets="diets"
+    />
+    <AppFooter />
   </div>
 </template>
 
