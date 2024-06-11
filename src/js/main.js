221897bd27f549b03d6ea282d76c54f50e3c8c72
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-06-11 10:20:37 +0200

    feat: setup app with VueJs, VueRouter, Axios, Bootstrap, Scss

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import { createApp } from "vue";
+
+// Import our custom CSS
+import "../scss/style.scss";
+
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
+import App from "../App.vue";
+
+createApp(App).mount("#app");
