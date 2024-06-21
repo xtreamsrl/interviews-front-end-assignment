f089e9f44c08b9fe109a5f843a46e9a2bbf6bb86
Rokas Stasiulis <rokastas@gmail.com>
2024-06-21 22:40:27 +0200

    added background color and made adjustments to the recipe card layout

--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -3,7 +3,7 @@ import { Recipe } from "../utils/types";
 
 const RecipeCard = ({ recipe }: { recipe: Recipe }) => {
   return (
-    <>
+    <div className="bg-white">
       <div className="h-80 relative">
         <Image
           src={recipe.image}
@@ -12,9 +12,11 @@ const RecipeCard = ({ recipe }: { recipe: Recipe }) => {
           objectFit="cover"
           />
       </div>
-      <h2 className="h-100 font-serif font-light text-4xl pt-4 pb-20">{recipe.name}</h2>
-      <p>Difficulty: {recipe.difficultyId}</p>
-    </>
+      <div className="p-4">
+        <h2 className="h-100 font-serif font-light text-4xl pb-20">{recipe.name}</h2>
+        <p>Difficulty: {recipe.difficultyId}</p>
+      </div>
+    </div>
   );
 };
 
