28057ef5ec431b52088356c00dfa02bd58a28822
Rokas Stasiulis <rokastas@gmail.com>
2024-06-22 20:43:14 +0200

    recipe modal layout updated

--- frontend/src/components/RecipeModal.tsx
+++ frontend/src/components/RecipeModal.tsx
@@ -44,26 +44,29 @@ const RecipeModal: React.FC<RecipeModalProps> = ({ recipe, onClose }) => {
   if (!recipe) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div id="recipe-modal-content" className="flex bg-white p-6">
-        <Image
-          src={recipe.image}
-          alt={recipe.name}
-          width={400}
-          height={800}
-        />
-        <div className="ml-6">
-          <button onClick={onClose} className="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 px-40 py-20">
+      <div id="recipe-modal-content" className="flex bg-white min-w-full min-h-full">
+        <div className="w-full relative">
+          <Image
+            src={recipe.image}
+            alt={recipe.name}
+            layout="fill"
+            objectFit="cover"
+            className="max-w-full h-full"
+          />
+        </div>
+        <div className="m-16 relative w-full">
+          <button onClick={onClose} className="absolute text-4xl top-2 right-2 text-gray-500 hover:text-gray-700">
             &times;
           </button>
-          <h2 className="text-2xl font-bold mb-4">{recipe.name}</h2>
-          <h3 className="text-xl font-semibold mb-2">Ingredients</h3>
-          <ul className="list-disc pl-5 mb-4">
+          <h2 className="h-100 font-serif text-7xl pb-20">{recipe.name}</h2>
+          <h3 className="text-2xl font-serif mb-6">Ingredients</h3>
+          <ul className="list-disc pl-5 mb-10">
             {recipe.ingredients.map((ingredient) => (
               <li key={ingredient}>{ingredient}</li>
             ))}
           </ul>
-          <h3 className="text-xl font-semibold mb-2">Instructions</h3>
+          <h3 className="text-2xl font-serif mb-6">Instructions</h3>
           <p>{recipe.instructions}</p>
         </div>
       </div>
