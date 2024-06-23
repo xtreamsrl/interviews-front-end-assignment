15cadced52659e8258fa02ef4fdc7b5289376a3b
Giovanni <giomiccio@live.it>
2024-06-23 17:43:52 +0200

    feat: implemented dishList with fetched datas

--- frontEnd/src/components/recipeCard/recipeCard.jsx
+++ frontEnd/src/components/recipeCard/recipeCard.jsx
@@ -1,53 +1,51 @@
 import './recipeCard.css'
 import Tag from './Tag.jsx'
-import DishCategory from './DishCategory.jsx'
 import DishIngredient from './DishIngredient.jsx'
 
-export default function RecipeCard() {
+export default function RecipeCard({ dishName, dishImg, dishRating, dishReviews, dishDifficulty, dishDiet, dishCuisine, dishIngredients }) {
+
 
     return (
         <div className='card'>
             <div className='cardLeft'>
-                <img src="./Beef-Tacos.jpg" className='dishImg'></img>
+                <img src={`http://localhost:8080${dishImg}`} className='dishImg'></img>
             </div>
             <div className='cardCenter'>
                 <div className='dishOverview'>
-                    <h2 className='dishTitle'>Tacos with meat</h2>
-                    <div className='dishCategories'>
-                        <DishCategory />
-                        <DishCategory />
-                    </div>
-                </div>
-                <div className='dishDetails'>
-                    <h3 className='dishType'>Classic Italian Pasta Dish</h3>
+                    <h2 className='dishTitle'>{dishName}</h2>
                     <div className='dishIngredients'>
-                        <DishIngredient />
-                        <DishIngredient />
+                        {
+                            dishIngredients.map((ingredient, i) => {
+                                console.log(i)
+                                return <DishIngredient
+                                    key={i}
+                                    dishIngredient={ingredient} />
+                            })
+                        }
                     </div>
                 </div>
+
+                <h3>Tags:</h3>
                 <div className='dishTags'>
-                    <Tag />
-                    <Tag />
+                    <Tag tagSubject={dishCuisine} />
+                    <Tag tagSubject={dishDiet} />
                 </div>
             </div>
             <div className='cardRight'>
                 <div className='reviewsBlock'>
                     <div className='reviewsText'>
-                        <h3 className='dishRating'>Rated 5.0 Stars</h3>
-                        <p className='numberOfReviews'>500+ reviews</p>
+                        <h3 className='dishRating'>Rated {dishRating} Stars</h3>
+                        <p className='numberOfReviews'>{dishReviews} {dishReviews > 1 ? " reviews" : " review"}</p>
                     </div>
                     <div className='reviewsVote'>
-                        <button className='redButton dishVote'>5.0</button>
+                        <button className='redButton dishVote'>{dishRating}</button>
                     </div>
                 </div>
                 <div className='actionBlock'>
-                    <h2 className='dishPrice'>$15</h2>
-                    <p className='dishTime'>30 Minutes, 4 Servings</p>
+                    <p className='dishDifficulty'><b>Recipe difficulty:</b> {dishDifficulty}</p>
                     <button className='redButton callToActionButton'>View details</button>
                 </div>
             </div>
         </div>
-
-
     )
 }
--- frontEnd/src/components/recipeList/RecipeList.jsx
+++ frontEnd/src/components/recipeList/RecipeList.jsx
@@ -1,8 +1,92 @@
+import { useEffect, useState } from "react";
 import RecipeCard from "../recipeCard/RecipeCard";
+import axios from 'axios'
 
 export default function RecipeList() {
+    const [recipe, setRecipe] = useState([]);
+    const [comment, setComment] = useState([])
+    const [difficulty, setDifficulty] = useState([])
+    const [diet, setDiet] = useState([])
+    const [cuisine, setCuisine] = useState([])
+    const [ingredient, setIngredient] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/recipes')
+            .then(res => setRecipe(res.data))
+            .catch(err => console.log(err));
+
+        axios.get('http://localhost:8080/comments')
+            .then(res => setComment(res.data))
+            .catch(err => console.log(err));
+
+        axios.get('http://localhost:8080/difficulties')
+            .then(res => setDifficulty(res.data))
+            .catch(err => console.log(err));
+
+        axios.get('http://localhost:8080/diets')
+            .then(res => setDiet(res.data))
+            .catch(err => console.log(err));
+
+        axios.get('http://localhost:8080/cuisines')
+            .then(res => setCuisine(res.data))
+            .catch(err => console.log(err));
+    }, []);
+
+
+    function getCurrentRecipe(recipeId) {
+        return recipe.find(recipe => recipe.id === recipeId)
+
+    }
+
+    function getRecipeComments(recipeId) {
+        return comment.filter(comment => comment.recipeId === recipeId);
+    }
+
+    function getRating(recipeId) {
+        const recipeComments = getRecipeComments(recipeId)
+
+        if (recipeComments.length === 0) return null;
+
+        const totalRating = recipeComments.reduce((acc, comment) => acc + comment.rating, 0);
+        return (totalRating / recipeComments.length).toFixed(1);
+    }
+
+    function getDifficulty(recipeId) {
+        const currentRecipe = getCurrentRecipe(recipeId)
+        const recipeDifficulty = difficulty.find(difficulty => difficulty.id === currentRecipe.difficultyId);
+        return recipeDifficulty.name
+    }
+
+    function getDiet(recipeId) {
+        const currentRecipe = getCurrentRecipe(recipeId)
+        const recipeDiet = diet.find(diet => diet.id === currentRecipe.dietId);
+        return recipeDiet.name
+    }
+
+    function getCuisine(recipeId) {
+        const currentRecipe = getCurrentRecipe(recipeId)
+        const recipeCuisine = cuisine.find(cuisine => cuisine.id === currentRecipe.dietId);
+        return recipeCuisine.name
+    }
 
     return (
-        <RecipeCard />
+        <>
+            {
+                recipe.map((dish) => {
+                    return <RecipeCard
+                        key={dish.id}
+                        dishName={dish.name}
+                        dishImg={dish.image}
+                        dishRating={getRating(dish.id)}
+                        dishReviews={getRecipeComments(dish.id).length}
+                        dishDifficulty={getDifficulty(dish.id)}
+                        dishDiet={getDiet(dish.id)}
+                        dishCuisine={getCuisine(dish.id)}
+                        dishIngredients={dish.ingredients}
+                    />
+                })
+            }
+
+        </>
     )
 }
