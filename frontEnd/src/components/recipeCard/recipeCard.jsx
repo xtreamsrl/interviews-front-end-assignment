2dcb36aaac78d92e9bbc2afad4c00c9e3e87e55d
Giovanni <giomiccio@live.it>
2024-06-24 11:30:57 +0200

    fix: changes to card layout

--- frontEnd/src/components/recipeCard/recipeCard.jsx
+++ frontEnd/src/components/recipeCard/recipeCard.jsx
@@ -7,43 +7,45 @@ export default function RecipeCard({ dishName, dishImg, dishRating, dishReviews,
 
     return (
         <div className='card'>
-            <div className='cardLeft'>
-                <img src={`http://localhost:8080${dishImg}`} className='dishImg'></img>
-            </div>
-            <div className='cardCenter'>
-                <div className='dishOverview'>
-                    <h2 className='dishTitle'>{dishName}</h2>
-                    <div className='dishIngredients'>
-                        {
-                            dishIngredients.map((ingredient, i) => {
-                                console.log(i)
-                                return <DishIngredient
-                                    key={i}
-                                    dishIngredient={ingredient} />
-                            })
-                        }
-                    </div>
+            <div className='cardContent'>
+                <div className='cardLeft'>
+                    <img src={`http://localhost:8080${dishImg}`} className='dishImg'></img>
                 </div>
+                <div className='cardCenter'>
+                    <div className='dishOverview'>
+                        <h2 className='dishTitle'>{dishName}</h2>
+                        <div className='dishIngredients'>
+                            {
+                                dishIngredients.map((ingredient, i) => {
+                                    console.log(i)
+                                    return <DishIngredient
+                                        key={i}
+                                        dishIngredient={ingredient} />
+                                })
+                            }
+                        </div>
+                    </div>
 
-                <h3>Tags:</h3>
-                <div className='dishTags'>
-                    <Tag tagSubject={dishCuisine} />
-                    <Tag tagSubject={dishDiet} />
-                </div>
-            </div>
-            <div className='cardRight'>
-                <div className='reviewsBlock'>
-                    <div className='reviewsText'>
-                        <h3 className='dishRating'>Rated {dishRating} Stars</h3>
-                        <p className='numberOfReviews'>{dishReviews} {dishReviews > 1 ? " reviews" : " review"}</p>
-                    </div>
-                    <div className='reviewsVote'>
-                        <button className='redButton dishVote'>{dishRating}</button>
+                    <h3>Tags:</h3>
+                    <div className='dishTags'>
+                        <Tag tagSubject={dishCuisine} />
+                        <Tag tagSubject={dishDiet} />
                     </div>
                 </div>
-                <div className='actionBlock'>
-                    <p className='dishDifficulty'><b>Recipe difficulty:</b> {dishDifficulty}</p>
-                    <button className='redButton callToActionButton'>View details</button>
+                <div className='cardRight'>
+                    <div className='reviewsBlock'>
+                        <div className='reviewsText'>
+                            <h3 className='dishRating'>Rated {dishRating} Stars</h3>
+                            <p className='numberOfReviews'>{dishReviews} {dishReviews > 1 ? " reviews" : " review"}</p>
+                        </div>
+                        <div className='reviewsVote'>
+                            <button className='redButton dishVote'>{dishRating}</button>
+                        </div>
+                    </div>
+                    <div className='actionBlock'>
+                        <p className='dishDifficulty'><b>Recipe difficulty:</b> {dishDifficulty}</p>
+                        <button className='redButton callToActionButton'>View details</button>
+                    </div>
                 </div>
             </div>
         </div>
