49a44fa04f9e195d947d9ba42c4a9764e6846974
Giovanni <giomiccio@live.it>
2024-06-24 11:33:30 +0200

    fix: changes to app layout

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import RecipeList from './components/recipeList/RecipeList.jsx'
 import NavBar from './components/navbar/Navbar.jsx'
+import LeftBar from './components/leftBar/LeftBar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,10 @@ function App() {
   return (
     <div className='app'>
       <NavBar />
-      <RecipeList />
+      <div className='pageBody'>
+        <LeftBar />
+        <RecipeList />
+      </div>
     </div>
   )
 }
